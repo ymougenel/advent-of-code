108b45041007f4373a5aee83699ec6fc762191f2
ymougenel <yann.mougenel@gmail.com>
2019-12-17 19:26:51 +0100

    feat(day6): part2

--- day6/day6.py
+++ day6/day6.py
@@ -37,10 +37,42 @@ def count_direct_orbits(graph):
     return count
 
 
+def get_path(graph, current, destination):
+    if current == destination:  # If found destination
+        return [destination]
+    elif not current in graph.keys():  # If dead end (graph leaf)
+        return []
+
+    for child in graph[current]:
+        path = get_path(graph, child, destination)
+        if path != []:  # Destination found among child
+            return [current] + path
+    return []  # Destination is not here
+
+
+def get_minimum_transfer(graph, origin='COM', p1='SAN', p2='YOU'):
+    # Trace 2 paths from graph origin to positions
+    path1 = get_path(graph, origin, p1)
+    path2 = get_path(graph, origin, p2)
+
+    min_transfert = len(path1) + len(path2)
+    # Look for intersections in paths
+    for element in path1:
+        if element in path2:
+            transferts_p1_intersection = path1.index(p1) - path1.index(element) - 1
+            transferts_p2_intersection = path2.index(p2) - path2.index(element) - 1
+            min_transfert = min(min_transfert, transferts_p1_intersection + transferts_p2_intersection)
+    return min_transfert
+
+
 if __name__ == '__main__':
     chain = read_file("inputs/day6.input")
     # chain = read_file("inputs/day6_example.input")
+    # chain = read_file("inputs/day6_example2.input")
     graph = init_graph(chain)
+
     c1 = count_direct_orbits(graph)
     c2 = count_indirect_orbits(graph)
     print(c1 + c2)
+
+    print(get_minimum_transfer(graph))
