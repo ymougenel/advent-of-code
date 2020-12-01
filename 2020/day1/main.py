9203b456cc0cb070b9ce1ef393bcdf122ecf24b4
ymougenel <yann.mougenel@gmail.com>
2020-12-01 08:41:15 +0100

    day1: part2

--- 2020/day1/main.py
+++ 2020/day1/main.py
@@ -11,12 +11,13 @@ def read_file(fileName):
 def find_entries_making_sum(entries, sum):
     for i in range(0, len(entries)):
         for j in range(i + 1, len(entries)):
-            if (entries[i] + entries[j] == sum):
-                return (entries[i], entries[j])
+            for k in range(j+1, len(entries)):
+                if (entries[i] + entries[j] + entries[k] == sum):
+                    return (entries[i], entries[j], entries[k])
 
 
 if __name__ == '__main__':
     # data = read_file("inputs/part1.example")
     data = read_file("inputs/part1.input")
-    (nb1, nb2) = find_entries_making_sum(data, 2020)
-    print(nb1 * nb2)
+    (nb1, nb2, nb3) = find_entries_making_sum(data, 2020)
+    print(nb1 * nb2 * nb3)
