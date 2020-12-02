522bb162f4f531c0ee0c2ca662588b4f9bad0fef
ymougenel <yann.mougenel@gmail.com>
2020-12-02 11:52:26 +0100

    day2: part2

--- 2020/day2/main.py
+++ 2020/day2/main.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
 
-
-def read_file(file_name):
+def count_valid_pass(file_name):
     with open(file_name) as input_file:
-        return [line for line in input_file.read().splitlines()]
+        return len([line for line in input_file.read().splitlines() if validate_position(line)])
 
 
-def validate(line):
+# Validate Password occurrence policy
+def validate_occurences(line):
     parts = line.split(" ")
     min = parts[0].split("-")[0]
     max = parts[0].split("-")[1]
     letter = parts[1][0]
-    occurence = parts[2].count(letter)
-    return int(min) <= occurence and occurence <= int(max)
+    occurrences = parts[2].count(letter)
+    return int(min) <= occurrences and occurrences <= int(max)
 
+
+# Validate Password position policy
+def validate_position(line):
+    parts = line.split(" ")
+    first_index = int(parts[0].split("-")[0]) - 1
+    last_index = int(parts[0].split("-")[1]) - 1
+    letter = parts[1][0]
+    password = parts[2][0:]
+
+    first_match = bool(password[first_index] == letter)
+    last_match = bool(last_index >= len(password) or password[last_index] == letter)
+    return first_match != last_match
 
 
 if __name__ == '__main__':
-    # data = read_file("inputs/part1.example")
-    data = read_file("inputs/part1.input")
-    valid_pass = 0
-    for line in data:
-        if (validate(line)):
-            valid_pass += 1
-    print(valid_pass)
+    # file = "inputs/part1.example"
+    file = "inputs/part1.input"
+    print(count_valid_pass(file))
