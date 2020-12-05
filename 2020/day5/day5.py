474a745cc4ab2a8aa52cf34dee21193ec6af3298
ymougenel <yann.mougenel@gmail.com>
2020-12-05 16:26:31 +0100

    feat(day5): finally solves it

--- 2020/day5/day5.py
+++ 2020/day5/day5.py
@@ -1,15 +1,48 @@
 #!/usr/bin/env python3
-import re
+import math
 
-def split_range(range,letter):
-    return ()
 
-def process_partitionning(passport):
-    return 0
+def split_range(range, letter):
+    middle = math.floor(range[0] + (range[1] - range[0]) / 2)
+    if letter == "F" or letter == "L":
+        return range[0], middle
+    else:
+        return middle + 1, range[1]
 
-def get_seat_id(row,col):
-    return 0
+
+def process_passport_translation(passport):
+    range = (0, 127)
+    for letter in passport[0:7]:
+        range = split_range(range, letter)
+    row = range[0]
+    if range[0] != range[1]:
+        print("Error:" + passport)
+    range = (0, 7)
+    for letter in passport[7:]:
+        range = split_range(range, letter)
+    col = range[0]
+    if range[0] != range[1]:
+        print("Error:" + passport)
+    return row, col
+
+
+def process_passport(passport):
+    row, col = process_passport_translation(passport)
+    return get_seat_id(row, col)
+
+
+def get_seat_id(row, col):
+    return row * 8 + col
+
+
+def read_file(file_name):
+    with open(file_name) as input_file:
+        return [line for line in input_file.read().splitlines()]
+
 
 if __name__ == '__main__':
-    print("Hi")
+    passports = read_file("inputs/part1.input")
+    result = [process_passport(seat) for seat in passports]
+    print(max(result))
 
+    print([missing for missing in range(0, 922) if missing not in result])
--- 2020/day5/day5_test.py
+++ 2020/day5/day5_test.py
@@ -4,6 +4,7 @@ import day5
 
 class day4_test(unittest.TestCase):
 
+
     def test_range_splitting(self):
         # F=Lower Half
         self.assertEqual(day5.split_range((0, 127), "F"), (0, 63))
@@ -15,9 +16,23 @@ class day4_test(unittest.TestCase):
 
         # B=UpperHalf
         self.assertEqual(day5.split_range((0, 127), "B"), (64, 127))
+        self.assertEqual(day5.split_range((64, 127), "B"), (96, 127))
+        self.assertEqual(day5.split_range((96, 127), "B"), (112, 127))
         self.assertEqual(day5.split_range((0, 63), "B"), (32, 63))
         self.assertEqual(day5.split_range((32, 47), "B"), (40, 47))
         self.assertEqual(day5.split_range((40, 47), "B"), (44, 47))
+
+
+    def test_process_passport_translation(self):
+        self.assertEqual(day5.process_passport_translation("FBFBBFFRLR"), (44, 5))
+
+
+    def test_process_passport(self):
+        self.assertEqual(day5.process_passport("BFFFBBFRRR"), 567)
+        self.assertEqual(day5.process_passport("FFFBBBFRRR"), 119)
+        self.assertEqual(day5.process_passport("BBFFBBFRLL"), 820)
+        self.assertEqual(day5.process_passport("FBFBBFFRLR"), 357)
+
 
 if __name__ == '__main__':
     unittest.main()
