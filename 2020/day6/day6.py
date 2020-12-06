342cbcc78226b7a635579eaabd0b54807b8b982e
ymougenel <yann.mougenel@gmail.com>
2020-12-06 11:58:44 +0100

    feat(day6): part2

--- 2020/day6/day6.py
+++ 2020/day6/day6.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 from collections import Counter
 
+
 def read_file(file_name):
     inputFile = open(file_name, "r")
     data = inputFile.read().splitlines()
     inputFile.close()
-    passports = []
+    groups = []
     i = 0
     for line in data:
         if line == "":
             i += 1
         else:
-            if len(passports) > i:
-                passports[i] += " " + line
-            else:
-                passports.append(" " + line)
-    return passports
+            if len(groups) <= i:
+                groups.append([])
+            groups[i].append(line)
+    return groups
 
 
-def count_yes(group_answer):
-    return len([ans for ans in Counter(group_answer).keys() if ans != " "])
+def count_anyone_yes(group_answer):
+    return len([ans for ans in Counter("".join(group_answer)).keys()])
+
+
+def count_everyone_yes(group_answer):
+    res = Counter("".join(group_answer))
+    return len([ans for ans in res if res[ans] == len(group_answer)])
 
 
 if __name__ == '__main__':
     # groups = read_file("inputs/part1.example")
     groups = read_file("inputs/part1.input")
-    print(groups)
-    print(sum([count_yes(yes) for yes in groups]))
+    print(sum([count_everyone_yes(ans) for ans in groups]))
