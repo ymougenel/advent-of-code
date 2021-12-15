418f547ac41331fc3e57cc67d715f106f150190c
ymougenel <yann.mougenel@gmail.com>
2021-12-15 18:20:56 +0300

    feat15: part1, Dikjstra implem

--- 2021/day15/main.py
+++ 2021/day15/main.py
@@ -20,12 +20,13 @@ def visit(vortex, map, info, visited=[]):
     for k in [-1, 1]:
         update_distance((i + k, j), vortex, distance_start, map, info)
         update_distance((i, j + k), vortex, distance_start, map, info)
-    visited.append(vortex)
+    current = info[vortex]
+    info[vortex] = (current[0],current[1], True)
 
 
 def solve(map, info):
     visited = []
-    info[(0, 0)] = (0, None)
+    info[(0, 0)] = (0, None, False)
     while info[(len(map)-1, len(map[0])-1)][0] == math.inf and len(visited) < len(map) * len(map[0]):
         closer_vortex = get_closer(info, visited)
         visit(closer_vortex, map, info, visited)
@@ -35,7 +36,7 @@ def get_closer(info, visited):
     min_dist = math.inf
     closer_vortex = None
     for vortex in info.keys():
-        if info[vortex][0] < min_dist and vortex not in visited:
+        if info[vortex][0] < min_dist and not info[vortex][2]:
             min_dist = info[vortex][0]
             closer_vortex = vortex
     return closer_vortex
@@ -46,7 +47,7 @@ def update_distance(child_vortex, parent_vortex, distance_start, map, info):
     if is_in_range(i, j, map):
         child_current_distance = info[child_vortex][0]
         if child_current_distance > distance_start + map[i][j]:
-            info[child_vortex] = (distance_start + map[i][j], parent_vortex)
+            info[child_vortex] = (distance_start + map[i][j], parent_vortex, info[child_vortex][2])
 
 
 def is_in_range(i, j, map):
@@ -59,10 +60,12 @@ if __name__ == '__main__':
     # Part 1
     map = read_file("inputs/part1.example")
     map = read_file("inputs/my_example")
+    map = read_file("inputs/part1.input")
     # print(map)
     for i in range(len(map)):
         for j in range(len(map[0])):
-            infos[(i, j)] = (math.inf, None)
+            infos[(i, j)] = (math.inf, None, False)
 
     solve(map, infos)
     print(infos[(len(map) - 1, len(map[0]) - 1)])
+    print("--- %s seconds ---" % (time.time() - start_time))
