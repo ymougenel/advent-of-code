85cf8a82fd1befc9c996d30624cadccdd48f2229
ymougenel <yann.mougenel@gmail.com>
2022-12-07 10:52:45 +0300

    feat day6: part2

--- 2022/day6/main.py
+++ 2022/day6/main.py
@@ -4,30 +4,31 @@ def read_file(file_name):
     with open(file_name) as input_file:
         return [line for line in input_file.read().splitlines()]
 
+
 def find_marker(data, LENGTH):
     count = 0
     for i in range(len(data)):
-        if data[i] in data[i+1:i+LENGTH]:
+        if data[i] in data[i + 1:i + LENGTH - count]:
             count = 0
         else:
             count += 1
         if count == LENGTH:
-            return i+1
+            return i + 1
     return 0
+
 
 def solve_part1(data):
     return find_marker(data, 4)
 
+
 def solve_part2(data):
     return find_marker(data, 14)
+
 
 if __name__ == '__main__':
     # Part 1
     data = read_file("inputs/part1.example")[0]
     data = read_file("inputs/part1.input")[0]
-    #data = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
-    #data = "nppdvjthqldpwncqszvftbrmjlhg"
-    #data = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
 
     print("Part 1: " + str(solve_part1(data)))
 
