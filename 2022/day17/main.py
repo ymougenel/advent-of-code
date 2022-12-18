2d9d13a06504726ec33235d7484fecc1b5a29c93
ymougenel <yann.mougenel@gmail.com>
2022-12-18 15:50:03 +0300

    feat: day17 wip

---
+++ 2022/day17/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def read_file(file_name):
+    with open(file_name) as input_file:
+        return [int(line) for line in input_file.read().splitlines()]
+
+def solve_part1(data):
+    return data
+
+def solve_part2(data):
+    return data
+
+if __name__ == '__main__':
+    # Part 1
+    data = read_file("inputs/part1.example")
+    #data = read_file("inputs/part1.input")
+    print("Part 1: " + str(solve_part1(data)))
+
+    # Part 2
+    data = read_file("inputs/part2.example")
+    #data = read_file("inputs/part2.input")
+    print("Part 2: " + str(solve_part2(data)))
