5046cd96b066452384524fb8fb512fed4674b136
ymougenel <yann.mougenel@gmail.com>
2022-12-19 10:32:44 +0300

    feat: day17 part1

--- 2022/template/main.py
+++ 2022/template/main.py
@@ -3,6 +3,8 @@ import time
 
 chamber = [["."] * 7 for k in range(3)]
 highest_rock = -1
+block_height = 0
+block_width = [0, 0]
 
 
 def read_file(file_name):
@@ -11,49 +13,55 @@ def read_file(file_name):
 
 
 def add_rock(id):
-    global chamber
+    global chamber, block_height, block_width
     if id % 5 == 0:
-        room_required = 1
+        block_height = 1
     elif id % 5 == 1:
-        room_required = 3
+        block_height = 3
     elif id % 5 == 2:
-        room_required = 3
+        roomblock_height_required = 3
     elif id % 5 == 3:
-        room_required = 4
+        block_height = 4
     else:
-        room_required = 2
+        block_height = 2
 
-    required_space = highest_rock + room_required + 4 - len(chamber)
+    required_space = highest_rock + block_height + 4 - len(chamber)
     if required_space > 0:
         for k in range(required_space):
             chamber.append(["."] * 7)
     if id % 5 == 0:  # Horizontal Rock
+        block_width = [2, 5]
         chamber[highest_rock + 4] = ["."] * 2 + [str(id)] * 4 + ["."]
         return highest_rock + 4
     elif id % 5 == 1:  # Cross Rock
+        block_width = [2, 4]
         chamber[highest_rock + 6][3] = str(id)
         chamber[highest_rock + 5] = ["."] * 2 + [str(id)] * 3 + ["."] * 2
         chamber[highest_rock + 4][3] = str(id)
         return highest_rock + 6
-    elif id % 5 == 2:
+    elif id % 5 == 2:  # L Inverse Rock
+        block_width = [2, 4]
         chamber[highest_rock + 6][4] = str(id)
         chamber[highest_rock + 5][4] = str(id)
         chamber[highest_rock + 4] = ["."] * 2 + [str(id)] * 3 + ["."] * 2
         return highest_rock + 6
-    elif id % 5 == 3:
+    elif id % 5 == 3:  # Vertical Rock
+        block_width = [2, 2]
         for k in range(4):
             chamber[highest_rock + 4 + k][2] = str(id)
         return highest_rock + 7
-    elif id % 5 == 4:
+    elif id % 5 == 4:  # Square Rock
+        block_width = [2, 3]
         chamber[highest_rock + 4] = ["."] * 2 + [str(id)] * 2 + ["."] * 3
         chamber[highest_rock + 5] = ["."] * 2 + [str(id)] * 2 + ["."] * 3
         return highest_rock + 5
 
 
 def move_rock(id, position, dist):
+    global block_width
     # Check rock can move (no items below)
-    for i in range(4):
-        for k in range(7):
+    for i in range(block_height):
+        for k in range(block_width[0], block_width[1] + 1):
             if position - i >= 0 and chamber[position - i][k] == id:  # Rock detected
                 if not 0 <= k + dist < 7:  # Border
                     return False
@@ -62,23 +70,26 @@ def move_rock(id, position, dist):
                     return False
 
     # Move rock
-    for i in range(4):
-        for k in range(7):
+    for i in range(block_height):
+        for k in range(block_width[0], block_width[1] + 1):
             if dist >= 1:
-                current = 7 - 1 - k
+                current = block_width[1] + block_width[0] - k
             else:
                 current = k
-            if position - 3 + i >= 0 and 0 <= current + dist < 7 and chamber[position - 3 + i][current] == id:
-                chamber[position - 3 + i][current] = "."
-                chamber[position - 3 + i][current + dist] = id
+            if position - i >= 0 and 0 <= current + dist < 7 and chamber[position - i][current] == id:
+                chamber[position - i][current] = "."
+                chamber[position - i][current + dist] = id
+    block_width[0] += dist
+    block_width[1] += dist
 
     return True
 
 
 def can_rock_fall(id, position):
     # Check rock can fall (no items below)
-    for i in range(4):
-        for k in range(7):
+    for i in range(block_height):
+        # for k in range(block_width[0], block_width[1] + 1):
+        for k in range(block_width[0], block_width[1] + 1):
             if position - i >= 0 and chamber[position - i][k] == id:
                 if position - i == 0:  # Bottom reached
                     return False
@@ -91,7 +102,7 @@ def can_rock_fall(id, position):
 def rock_fall(id, position):
     # Move piece down
     for i in range(4):
-        for k in range(7):
+        for k in range(block_width[0], block_width[1] + 1):
             if position - 3 + i >= 0 and chamber[position - 3 + i][k] == id:
                 chamber[position - 3 + i][k] = "."
                 chamber[position - 4 + i][k] = id
@@ -102,7 +113,7 @@ def rock_fall(id, position):
 def run(data, turns):
     global highest_rock
     gas_index = 0
-    for i in range(turns):
+    for i in range(2022):
         position = add_rock(i)
         # display()
         if data[gas_index % len(data)] == ">":
@@ -125,7 +136,7 @@ def run(data, turns):
         # display()
 
 
-def solve_part1(data, turns = 2022):
+def solve_part1(data, turns=2022):
     run(data, turns)
 
     return highest_rock + 1
@@ -145,7 +156,7 @@ if __name__ == '__main__':
     start_time = time.time()
     # Part 1
     data = read_file("inputs/part1.example")
-    # data = read_file("inputs/part1.input")
+    data = read_file("inputs/part1.input")
     print("Part 1: " + str(solve_part1(data)))
     print("Solved in : ", (time.time() - start_time))
     # Part 2
--- 2022/template/main_test.py
+++ 2022/template/main_test.py
@@ -5,7 +5,7 @@ class main_test(unittest.TestCase):
 
     def test_input_example_1(self):
         data = main.read_file("inputs/part1.example")
-        self.assertEqual(3068, main.solve_part1(data, 2022))
+        self.assertEqual(3068, main.solve_part1(data))
 
     def test_input_1(self):
         data = main.read_file("inputs/part1.input")
