e06a11de1bf26a0bc882c18765e11279950b6fe0
ymougenel <yann.mougenel@gmail.com>
2023-12-13 22:06:05 +0100

    WIP day12 part2

--- 2023/day12/main.py
+++ 2023/day12/main.py
@@ -7,36 +7,36 @@ def read_file(file_name):
         return [line for line in input_file.read().splitlines()]
 
 
-def is_possibility(springs, counts):
-    blocks = 0
-    for elt in springs:
-        if elt == "#":
-            blocks += 1
-        elif elt == ".":
-            if blocks != 0:
-                if len(counts) == 0 or blocks != counts[0]:
-                    return False
-                else:
-                    blocks = 0
-                    counts = counts[1:]
-        else:
-            print("Error, found unwanted symbol")
-            return -1
-    return (len(counts) == 0 and blocks == 0) or (len(counts) == 1 and blocks == counts[0])
+# def is_possibility(springs, counts):
+#     blocks = 0
+#     for elt in springs:
+#         if elt == "#":
+#             blocks += 1
+#         elif elt == ".":
+#             if blocks != 0:
+#                 if len(counts) == 0 or blocks != counts[0]:
+#                     return False
+#                 else:
+#                     blocks = 0
+#                     counts = counts[1:]
+#         else:
+#             print("Error, found unwanted symbol")
+#             return -1
+#     return (len(counts) == 0 and blocks == 0) or (len(counts) == 1 and blocks == counts[0])
 
 
-def find_possibilities(springs):
-    possibilities = [""]
-    for i in range(len(springs)):
-        tmp_poss = []
-        for poss in possibilities:
-            if springs[i] == "?":
-                tmp_poss.append(poss + "#")
-                tmp_poss.append(poss + ".")
-            else:
-                tmp_poss.append(poss + springs[i])
-        possibilities = tmp_poss
-    return possibilities
+# def find_possibilities(springs):
+#     possibilities = [""]
+#     for i in range(len(springs)):
+#         tmp_poss = []
+#         for poss in possibilities:
+#             if springs[i] == "?":
+#                 tmp_poss.append(poss + "#")
+#                 tmp_poss.append(poss + ".")
+#             else:
+#                 tmp_poss.append(poss + springs[i])
+#         possibilities = tmp_poss
+#     return possibilities
 
 
 def parse_springs(line):
@@ -45,17 +45,56 @@ def parse_springs(line):
     return springs, counts
 
 
-def solve_part1(data):
+def solve_part(data):
     total = 0
-    for springs, counts in data:
-        possibilities = find_possibilities(springs)
-        possibles = [poss for poss in possibilities if is_possibility(poss, counts)]
-        total += len(possibles)
+    i = 0
+    for spring, counts in data:
+        possibilities = parse_opti_springs(spring, counts, sum(counts), spring.count("#"))
+        i += 1
+        print(i)
+        total += len(possibilities)
     return total
 
+# '.#.###.#.######'
+def parse_opti_springs(line, counts, total_required, already_placed, parsed="", block_count=0, i=0):
+    if i >= len(line):
+        if (block_count == 0 and len(counts) == 0) or (len(counts) == 1 and block_count == counts[0]):
+            return [parsed]
+        else:
+            return []
+    if line[i] == "#":
+        sub_sols = parse_opti_springs(line, counts, total_required, already_placed, parsed + "#", block_count + 1,
+                                      i + 1)
+        return sub_sols
+    elif line[i] == ".":
+        if block_count == 0:
+            sub_sols = parse_opti_springs(line, counts, total_required, already_placed, parsed + ".", block_count,
+                                          i + 1)
+            return sub_sols
+        else:
+            if len(counts) == 0 or block_count != counts[0]:
+                return []
+            else:
+                sub_sols = parse_opti_springs(line, counts[1:], total_required, already_placed, parsed + ".", 0, i + 1)
+                return sub_sols
+    else:
+        sub_sols1 = parse_opti_springs(line[:i] + "." + line[i + 1:], counts, total_required, already_placed, parsed,
+                                       block_count, i)
+        sub_sols2 = []
+        if already_placed <= total_required:
+            sub_sols2 = parse_opti_springs(line[:i] + "#" + line[i + 1:], counts, total_required, already_placed + 1,
+                                           parsed, block_count, i)
+        return sub_sols1 + sub_sols2
 
-def solve_part2(data):
-    return data
+
+def unfold(data, time=5):
+    new_data = []
+    for line, counts in data:
+        new_line = ""
+        for i in range(time):
+            new_line += line + "?"
+        new_data.append((new_line[:-1], counts * time))
+    return new_data
 
 
 if __name__ == '__main__':
@@ -65,10 +104,11 @@ if __name__ == '__main__':
     data = read_file("inputs/input.txt")
     data = [parse_springs(line) for line in data]
 
-    print("Part 1: " + str(solve_part1(data)))
-    print("Solved in : ", (time.time() - start_time))
+    print("Part 1: " + str(solve_part(data)))
+    print("Part1 Solved in : ", (time.time() - start_time))
 
     # Part 2
-    data = read_file("inputs/example2.txt")
-    # data = read_file("inputs/input.txt")
-    print("Part 2: " + str(solve_part2(data)))
+    start_time = time.time()
+    data = unfold(data)
+    print("Part 2: " + str(solve_part(data)))
+    print("Part 2 Solved in : ", (time.time() - start_time))
--- 2023/day12/main_test.py
+++ 2023/day12/main_test.py
@@ -5,32 +5,38 @@ import main
 class main_test(unittest.TestCase):
     # ???..????# 1,3,1
 
-    def test_is_possible(self):
-        """  ???..????# 1,3,1 """
-        self.assertEqual(True, main.is_possibility("#...###.#", [1, 3, 1]))
-        self.assertEqual(True, main.is_possibility(".#..###.#", [1, 3, 1]))
-        self.assertEqual(True, main.is_possibility("..#.###.#", [1, 3, 1]))
-        self.assertEqual(False, main.is_possibility("##..###.#", [1, 3, 1]))
-
-        """ ??.?#?"""
-        self.assertEqual(True, main.is_possibility("#..##.", [1, 2]))
-        self.assertEqual(True, main.is_possibility(".#.##.", [1, 2]))
-        self.assertEqual(True, main.is_possibility("#...##", [1, 2]))
-        self.assertEqual(True, main.is_possibility(".#..##", [1, 2]))
-        self.assertEqual(False, main.is_possibility("##.##.", [1, 2]))
-        self.assertEqual(False, main.is_possibility("...##.", [1, 2]))
-        self.assertEqual(False, main.is_possibility(".#.###", [1, 2]))
-
-    def test_input_example_1(self):
-        data = main.read_file("inputs/example1.txt")
-        data = [main.parse_springs(line) for line in data]
-        self.assertEqual(21, main.solve_part1(data))
-
-
-    def test_input_part_1(self):
-        data = main.read_file("inputs/input.txt")
-        data = [main.parse_springs(line) for line in data]
-        self.assertEqual(7191, main.solve_part1(data))
+    def test_parse_opti_springs(self):
+        self.assertEqual([".#"], main.parse_opti_springs(".?", [1], 1, 0))
+        self.assertEqual(["..#", "#.."], main.parse_opti_springs("?.?", [1], 1, 0))
+        self.assertEqual(['.#..##', '.#.##.', '#...##', '#..##.'], main.parse_opti_springs("??.?#?", [1, 2], 3, 1))
+        self.assertEqual(['.#.###.#.######'], main.parse_opti_springs("?#?#?#?#?#?#?#?", [1, 3, 1, 6], 11, 7))
+        # self.assertEqual(1, len(main.parse_opti_springs("?#?#?#?#?#?#?#?", [1, 3, 1, 6], 11, 7)))
+    # def test_is_possible(self):
+    #     """  ???..????# 1,3,1 """
+    #     self.assertEqual(True, main.is_possibility("#...###.#", [1, 3, 1]))
+    #     self.assertEqual(True, main.is_possibility(".#..###.#", [1, 3, 1]))
+    #     self.assertEqual(True, main.is_possibility("..#.###.#", [1, 3, 1]))
+    #     self.assertEqual(False, main.is_possibility("##..###.#", [1, 3, 1]))
+    #
+    #     """ ??.?#?"""
+    #     self.assertEqual(True, main.is_possibility("#..##.", [1, 2]))
+    #     self.assertEqual(True, main.is_possibility(".#.##.", [1, 2]))
+    #     self.assertEqual(True, main.is_possibility("#...##", [1, 2]))
+    #     self.assertEqual(True, main.is_possibility(".#..##", [1, 2]))
+    #     self.assertEqual(False, main.is_possibility("##.##.", [1, 2]))
+    #     self.assertEqual(False, main.is_possibility("...##.", [1, 2]))
+    #     self.assertEqual(False, main.is_possibility(".#.###", [1, 2]))
+    #
+    # def test_input_example_1(self):
+    #     data = main.read_file("inputs/example1.txt")
+    #     data = [main.parse_springs(line) for line in data]
+    #     self.assertEqual(21, main.solve_part1(data))
+    #
+    #
+    # def test_input_part_1(self):
+    #     data = main.read_file("inputs/input.txt")
+    #     data = [main.parse_springs(line) for line in data]
+    #     self.assertEqual(7191, main.solve_part1(data))
     #
     # def test_input_example_2(self):
     #     data = main.read_file("inputs/example2.txt")
