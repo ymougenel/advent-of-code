a77794535651e2fd9d259d914183372d3fecde63
ymougenel <yann.mougenel@gmail.com>
2023-12-15 18:29:56 +0100

    feat: day5 part2

--- 2023/day5/main.py
+++ 2023/day5/main.py
@@ -41,23 +41,36 @@ def parse_almanac(data):
             dest, src, rg = list(map(int, line.split()))
             current.append((dest, src, rg))
 
+    # Sort the lists values (based on src)
+    seed_to_soil.sort(key=lambda x: x[1])
+    soil_to_fertilizer.sort(key=lambda x: x[1])
+    fertilizer_to_water.sort(key=lambda x: x[1])
+    water_to_light.sort(key=lambda x: x[1])
+    light_to_temperature.sort(key=lambda x: x[1])
+    temperature_to_humidity.sort(key=lambda x: x[1])
+    humidity_to_location.sort(key=lambda x: x[1])
+
 
 def get_location(seed):
-    val = get_value(seed_to_soil, seed)
-    val = get_value(soil_to_fertilizer, val)
-    val = get_value(fertilizer_to_water, val)
-    val = get_value(water_to_light, val)
-    val = get_value(light_to_temperature, val)
-    val = get_value(temperature_to_humidity, val)
-    val = get_value(humidity_to_location, val)
-    return val
+    # Retrieve the value for each list, among with the skipping value
+    val, skipping1 = get_value(seed_to_soil, seed)
+    val, skipping2 = get_value(soil_to_fertilizer, val)
+    val, skipping3 = get_value(fertilizer_to_water, val)
+    val, skipping4 = get_value(water_to_light, val)
+    val, skipping5 = get_value(light_to_temperature, val)
+    val, skipping6 = get_value(temperature_to_humidity, val)
+    val, skipping7 = get_value(humidity_to_location, val)
+    return val, min(skipping1, skipping2, skipping3, skipping4, skipping5, skipping6, skipping7)
 
 
 def get_value(lst, key):
+    closest = 1
     for dest, src, rg in lst:
         if src <= key < src + rg:
-            return dest + key - src
-    return key
+            return dest + key - src, src + rg - key
+        if key < src:
+            closest = src
+    return key, closest
 
 
 def solve_part1():
@@ -69,11 +82,13 @@ def solve_part2():
     minimum = math.inf
     while i < len(seeds):
         begin = seeds[i]
-        length = seeds[i+1]
-        for seed in range(begin,begin+length):
-            minimum = min(minimum, get_location(seed))
+        length = seeds[i + 1]
+        seed = begin
+        while seed < begin + length:
+            mini, skipping = get_location(seed)
+            minimum = min(minimum, mini)
+            seed += skipping
         i += 2
-        print(i)
     return minimum
 
 
@@ -87,6 +102,4 @@ if __name__ == '__main__':
     print("Solved in : ", (time.time() - start_time))
 
     # Part 2
-    data = read_file("inputs/example1.txt")
-    data = read_file("inputs/input.txt")
     print("Part 2: " + str(solve_part2()))
