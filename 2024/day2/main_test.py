dd5f672e7c0974f903c27d5192c0bfa85067f0f6
ymougenel <yann.mougenel@gmail.com>
2024-12-02 13:01:07 +0100

    feat: day2 part 2

--- 2024/day2/main_test.py
+++ 2024/day2/main_test.py
@@ -1,12 +1,11 @@
 import unittest
 import main
 
-
 class main_test(unittest.TestCase):
 
     def test_input_example_1(self):
         data = main.read_file("inputs/example1.txt")
-        # self.assertEqual(2, main.solve_part1(data))
+        self.assertEquals(2, main.solve_part(data))
 
     def test_increase_decrease_function(self):
         data = main.read_file("inputs/example1.txt")
@@ -19,12 +18,19 @@ class main_test(unittest.TestCase):
 
     def test_input_part_1(self):
         data = main.read_file("inputs/input.txt")
-        self.assertEqual(479, main.solve_part1(data))
+        self.assertEqual(479, main.solve_part(data))
 
+    def test_increase_decrease_function_tolerance(self):
+        data = main.read_file("inputs/example1.txt")
+        self.assertTrue(main.check_increase_decrease_range([1, 5, 2, 3], tolerate=1))
+        self.assertTrue(main.check_increase_decrease_range([1, 3, 2, 3], tolerate=1))
+        self.assertTrue(main.check_increase_decrease_range([1, 3, 1, 4], tolerate=1))
+        self.assertTrue(main.check_increase_decrease_range([1, 3, 4, 9], tolerate=1))
+        self.assertTrue(main.check_increase_decrease_range([5, 3, 4, 5], tolerate=1))
 
-    # def test_input_example_2(self):
-    #     data = main.read_file("inputs/example2.txt")
-    #     self.assertEqual(0, main.solve_part2(data))
+    def test_input_example_2(self):
+        data = main.read_file("inputs/example1.txt")
+        self.assertEqual(4, main.solve_part(data, tolerate=1))
 
 
 if __name__ == '__main__':
