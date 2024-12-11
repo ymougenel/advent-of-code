8af2fb5f9d7bcefafba859c9b322b41d23d3b031
ymougenel <ymougenel@takima.fr>
2024-12-11 23:55:12 +0100

    feat: day 11 part 2

--- 2024/day11/main.py
+++ 2024/day11/main.py
@@ -7,16 +7,6 @@ def read_file(file_name):
         return [int(i) for i in input_file.read().splitlines()[0].split(" ")]
 
 
-def run_simulation(rocks, iterations):
-    k = 0
-    while k < iterations:
-        new_rock = []
-        for i in range(len(rocks)):
-            new_rock += rock_blinkink(rocks[i])
-        rocks = new_rock
-        k += 1
-    return rocks
-
 def rock_blinkink(rock):
     if rock == 0:
         return [1]
@@ -29,19 +19,47 @@ def rock_blinkink(rock):
 
 
 def solve_part1(rocks):
-    rocks = run_simulation(rocks,25)
-    print(rocks)
+    k = 0
+    while k < 25:
+        new_rock = []
+        for i in range(len(rocks)):
+            new_rock += rock_blinkink(rocks[i])
+        rocks = new_rock
+        k += 1
     return len(rocks)
 
 
-def solve_part2(data):
-    return data
+def get_state(rocks):
+    current_state = {}
+    for rock in rocks:
+        if rock in current_state:
+            current_state[rock] += 1
+        else:
+            current_state[rock] = 1
+    return current_state
+
+
+def solve_part2(rocks):
+    k = 0
+    current_state = get_state(rocks)
+    while k < 75:
+        next_state = {}
+        for rock, count in current_state.items():
+            rock_simu = rock_blinkink(rock)
+            for res_rock in rock_simu:
+                if res_rock in next_state:
+                    next_state[res_rock] += count
+                else:
+                    next_state[res_rock] = count
+        current_state = next_state
+        k += 1
+    return sum(current_state.values())
 
 
 if __name__ == '__main__':
     # Parse input file
-    data = read_file("inputs/example1.txt")
-    data = read_file("inputs/example2.txt")
+    # data = read_file("inputs/example1.txt")
+    # data = read_file("inputs/example2.txt")
     data = read_file("inputs/input.txt")
 
     # Part 1
--- 2024/day11/main_test.py
+++ 2024/day11/main_test.py
@@ -5,15 +5,18 @@ class main_test(unittest.TestCase):
 
     def test_input_example_1(self):
         data = main.read_file("inputs/example1.txt")
-        self.assertEqual(0, main.solve_part1(data))
+        self.assertEqual(125681, main.solve_part1(data))
 
     def test_input_example_2(self):
         data = main.read_file("inputs/example2.txt")
-        self.assertEqual(0, main.solve_part2(data))
+        self.assertEqual(55312, main.solve_part1(data))
+    # def test_input_example_2(self):
+    #     data = main.read_file("inputs/example2.txt")
+    #     self.assertEqual(0, main.solve_part2(data))
 
-    # def test_input_1(self):
-    #     data = main.read_file("inputs/input.txt")
-    #     self.assertEqual(0, main.solve_part1(data))
+    def test_input_1(self):
+        data = main.read_file("inputs/input.txt")
+        self.assertEqual(186175, main.solve_part1(data))
 
     # def test_input_2(self):
     #     data = main.read_file("inputs/input.txt")
