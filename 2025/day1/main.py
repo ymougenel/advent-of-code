4de230c978e1b07f7465fcfa060254bd6fce7efa
ymougenel <ymougenel@takima.fr>
2025-12-01 13:59:43 +0100

    feat: day1 part2

--- 2025/day1/main.py
+++ 2025/day1/main.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python3
 import time
 
+
 def read_file(file_name):
     with open(file_name) as input_file:
         lines = [line for line in input_file.read().splitlines()]
-        return [(l[0],int(l[1:])) for l in lines]
+        return [(l[0], int(l[1:])) for l in lines]
+
 
 def solve_part1(data):
     current = 50
     max = 100
     encountered_zero = 0
-    for a,v in data:
-        if a=='L':
+    for a, v in data:
+        if a == 'L':
             current = (current - v) % max
-        elif a=='R':
+        elif a == 'R':
             current = (current + v) % max
-        else:
-            print("This should not happend")
         if current == 0:
             encountered_zero += 1
         # print(a,v,current)
     return encountered_zero
 
+
 def solve_part2(data):
-    return data
+    current = 50
+    max = 100
+    encountered_zero = 0
+    for a, v in data:
+        for i in range(0,v):
+            if a == 'L':
+                current -= 1
+            elif a == 'R':
+                current += 1
+            current = current % max
+            if current == 0:
+                encountered_zero += 1
+    return encountered_zero
 
 if __name__ == '__main__':
-
     # Parse input file
     data = read_file("inputs/example1.txt")
-    # data = read_file("inputs/example2.txt")
     data = read_file("inputs/input.txt")
 
     # Part 1
@@ -41,4 +52,3 @@ if __name__ == '__main__':
     start_time = time.time()
     print("Part 2: " + str(solve_part2(data)))
     print("-> Part2 solved in: ", (time.time() - start_time))
-
--- 2025/day1/main_test.py
+++ 2025/day1/main_test.py
@@ -1,23 +1,57 @@
 import unittest
 import main
 
+
 class main_test(unittest.TestCase):
 
     def test_input_example_1(self):
         data = main.read_file("inputs/example1.txt")
-        self.assertEqual(0, main.solve_part1(data))
+        self.assertEqual(3, main.solve_part1(data))
+
+    def test_input_1(self):
+        data = main.read_file("inputs/input.txt")
+        self.assertEqual(1066, main.solve_part1(data))
 
     def test_input_example_2(self):
-        data = main.read_file("inputs/example2.txt")
-        self.assertEqual(0, main.solve_part2(data))
+        data = main.read_file("inputs/example1.txt")
+        self.assertEqual(6, main.solve_part2(data))
 
-    # def test_input_1(self):
-    #     data = main.read_file("inputs/input.txt")
-    #     self.assertEqual(0, main.solve_part1(data))
+    def test_overflow(self):
+        data = [('R', 60)]
+        self.assertEqual(1, main.solve_part2(data))
 
-    # def test_input_2(self):
-    #     data = main.read_file("inputs/input.txt")
-    #     self.assertEqual(0, main.solve_part2(data))
+    def test_big_overflow(self):
+        data = [('R', 1000)]
+        self.assertEqual(10, main.solve_part2(data))
+
+    def test_extend_2(self):
+        data = [('L', 50), ('R', 10)]
+        self.assertEqual(1, main.solve_part2(data))
+
+    def test_corner_cases(self):
+        data = [('L', 50)]
+        self.assertEqual(1, main.solve_part2(data))
+        data = [('L', 50), ('L', 100)]
+        self.assertEqual(2, main.solve_part2(data))
+        data = [('L', 50), ('R', 100)]
+        self.assertEqual(2, main.solve_part2(data))
+        data = [('R', 50)]
+        self.assertEqual(1, main.solve_part2(data))
+        data = [('L', 50), ('R', 200)]
+        self.assertEqual(3, main.solve_part2(data))
+        data = [('L', 50), ('L', 200)]
+        self.assertEqual(3, main.solve_part2(data))
+        data = [('L', 200)]
+        self.assertEqual(2, main.solve_part2(data))
+        data = [('R', 200)]
+        self.assertEqual(2, main.solve_part2(data))
+        data = [('L', 150)]
+        self.assertEqual(2, main.solve_part2(data))
+
+    def test_input_2(self):
+        data = main.read_file("inputs/input.txt")
+        self.assertEqual(6223, main.solve_part2(data))
+
 
 if __name__ == '__main__':
     unittest.main()
