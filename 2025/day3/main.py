0c550b213cf26dd828e82b4a5730f8aee144488c
ymougenel <yann.mougenel@gmail.com>
2025-12-04 13:33:21 +0100

    feat day3 part2

--- 2025/day3/main.py
+++ 2025/day3/main.py
@@ -18,8 +18,27 @@ def get_largest_joltage(bat_line):
     second_largest = max(bat_line[index_maxi+1:])
     return 10*maxi + second_largest
 
-def solve_part2(data):
-    return data
+def solve_part2(batteries):
+    res = 0
+    for bat_line in batteries:
+        s = get_friction_largest_joltage(bat_line)
+        res += s
+    return res
+
+def get_friction_largest_joltage(bat_line):
+    res = ""
+    count_remaining_digit = 12
+    block = bat_line
+    while count_remaining_digit >0:
+        max_block = block[:len(block) - count_remaining_digit+1 ]
+        remaining_block = block[-(count_remaining_digit-1):]
+        maxi = max(max_block)
+        res += str(maxi)
+        index_maxi = max_block.index(maxi)
+        block = max_block[index_maxi+1:] + remaining_block
+        count_remaining_digit -= 1
+
+    return int(res)
 
 if __name__ == '__main__':
 
