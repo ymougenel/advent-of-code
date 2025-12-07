836dc3fbde1fa9271680d22f166125d1cc119336
ymougenel <ymougenel@takima.fr>
2025-12-07 15:29:43 +0100

    feat: day7 part2

--- 2025/day7/main.py
+++ 2025/day7/main.py
@@ -8,14 +8,11 @@ def read_file(file_name):
 
 
 def solve_part1(maze):
-    total_count = 0
-    for i in range(1, len(maze)):
-        total_count += run_line(maze, i)
-        # print_maze(maze)
-    return total_count
+    # Process line by line, and for each line counts occurred splitting
+    return sum([run_classical_line(maze, i) for i in range(1, len(maze))])
 
 
-def run_line(maze, i):
+def run_classical_line(maze, i):
     count = 0
     for j in range(len(maze[0])):
         if maze[i - 1][j] in ['S', '|']:
@@ -29,13 +26,76 @@ def run_line(maze, i):
                 maze[i][j] = "|"
     return count
 
+
 def print_maze(maze):
     for line in maze:
         print(line)
     print("\n")
 
-def solve_part2(data):
-    return None
+
+def solve_part2(maze):
+    computed_path = {}
+    for i in range(1, len(maze)):
+        run_quantum_line(maze, i, computed_path)
+
+
+    all_final_paths = [maze[-1][j] for j in range(len(maze[0])) if maze[-1][j] != "."]
+    count_timelines = 0
+    for final_paths in all_final_paths:
+        for path in final_paths:
+            count_timelines += computed_path[path]
+    return count_timelines
+
+
+def run_quantum_line(maze, i, computed_path):
+    # Process all line elements
+    for j in range(len(maze[0])):
+
+        # If below starting point
+        if maze[i - 1][j] == "S":
+            maze[i][j] = [(i - 1, j)]
+            computed_path[(i - 1, j)] = 1
+
+        # Elif below quantum timeline
+        elif maze[i - 1][j] not in [".", "^"]:
+
+            # If current element is splitter
+            if maze[i][j] == "^":
+                # Split the signal, and mark the splitter origin
+                mark_splitter_path(i, j, maze[i - 1][j], computed_path)
+
+                # Process left
+                if maze[i][j - 1] == ".":
+                    maze[i][j - 1] = [(i, j)]
+                else:
+                    maze[i][j - 1].append((i, j))
+
+                # Process right
+                if maze[i][j + 1] == ".":
+                    maze[i][j + 1] = [(i, j)]
+                else:
+                    maze[i][j + 1].append((i, j))
+
+            # Below timeline but not splitter
+            else:
+                # If "." found -> >The Path is the same as previous
+                if maze[i][j] == ".":
+                    maze[i][j] = maze[i - 1][j]
+                # If post splitter -> The path is the concatenation of left splitter and previous path
+                else:
+                    maze[i][j] += maze[i - 1][j]
+
+
+'''
+ Process the new splitter timeline_count.
+ The split count will be the sum of all the paths from previous splitter
+'''
+def mark_splitter_path(i, j, previous_path, computed_path):
+    if (i, j) not in computed_path:
+        computed_path[(i, j)] = 0
+
+    for path in previous_path:
+        computed_path[(i, j)] += computed_path[path]
 
 
 if __name__ == '__main__':
@@ -46,7 +106,7 @@ if __name__ == '__main__':
 
     # Part 1
     start_time = time.time()
-    print("Part 1: " + str(solve_part1(data)))
+    # print("Part 1: " + str(solve_part1(data)))
     print("-> Part1 solved in: ", (time.time() - start_time))
 
     # Part 2
--- 2025/day7/main_test.py
+++ 2025/day7/main_test.py
@@ -5,19 +5,19 @@ class main_test(unittest.TestCase):
 
     def test_input_example_1(self):
         data = main.read_file("inputs/example1.txt")
-        self.assertEqual(0, main.solve_part1(data))
+        self.assertEqual(21, main.solve_part1(data))
 
     def test_input_example_2(self):
-        data = main.read_file("inputs/example2.txt")
-        self.assertEqual(0, main.solve_part2(data))
+        data = main.read_file("inputs/example1.txt")
+        self.assertEqual(40, main.solve_part2(data))
 
-    # def test_input_1(self):
-    #     data = main.read_file("inputs/input.txt")
-    #     self.assertEqual(0, main.solve_part1(data))
+    def test_input_1(self):
+        data = main.read_file("inputs/input.txt")
+        self.assertEqual(1698, main.solve_part1(data))
 
-    # def test_input_2(self):
-    #     data = main.read_file("inputs/input.txt")
-    #     self.assertEqual(0, main.solve_part2(data))
+    def test_input_2(self):
+        data = main.read_file("inputs/input.txt")
+        self.assertEqual(95408386769474, main.solve_part2(data))
 
 if __name__ == '__main__':
     unittest.main()
